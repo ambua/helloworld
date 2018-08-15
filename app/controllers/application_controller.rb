120d6d567235097625bade92ebde40cd4e46294d
bodabie <you@example.com>
2018-08-15 08:42:03 +0200

    Hello on dialect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
 
     def hello
-      render html: "hello, world! ¡Hola, mundo!"
+      render html: "hello, world! ¡Hola, mundo! Sälü Zäme!"
     end
 end
